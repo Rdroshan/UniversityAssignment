d7903c843e628c982278d9ee94f1058d0759b296
Rdroshan <roshan.dawande@vogo.in>
2021-07-12 13:10:30 +0530

    :zap: hiding the mongodb url

--- clients/mongo.py
+++ clients/mongo.py
@@ -1,6 +1,7 @@
 import pymongo
+from django.conf import settings
 import certifi
 # To reference the installed certificate authority
 ca = certifi.where()
 
-client = pymongo.MongoClient('mongodb+srv://prodigal_be_test_01:prodigaltech@test-01-ateon.mongodb.net/sample_training', tlsCAFile=ca)
+client = pymongo.MongoClient(settings.MONGODB['HOST'], tlsCAFile=ca)
